f84f71af4743a6312e0e564ea5a86cee969524f0
Brian Staruk <brian@staruk.net>
2024-04-14 09:32:08 -0400

    website buildout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
     fontFamily: {
-      sans: '"PT Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
-      serif: '"PT Serif", "Palatino", "Palatino Linotype", "Palatino LT STD", "Book Antiqua", "Georgia", serif',
+      sans: '"Lato", sans-serif',
+      serif: '"Merriweather", serif',
     },
   },
   plugins: [require('@tailwindcss/forms')],
-} satisfies Config
-
+} satisfies Config;
